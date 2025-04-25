e7eacb7f73d7496f074faf05a62f93d996112f5b
violettayun <violetta.yun13@gmail.com>
2025-04-25 19:40:42 +0500

    BST

---
+++ src/BST.java
@@ -0,0 +1,125 @@
+import java.util.*;
+
+public class BST<K extends Comparable<K>, V> implements Iterable<BST.Entry<K, V>> {
+    private Node root;
+    private int size = 0; //1. Add siz
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    //3. Access both key and value during iteration
+    public static class Entry<K, V> {
+        private final K key;
+        private final V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() { return key; }
+        public V getValue() { return value; }
+    }
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) {
+            size++;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        return x;
+    }
+
+    public V get(K key) {
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.val;
+        }
+        return null;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            size--;
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node temp = x;
+            x = min(temp.right);
+            x.right = deleteMin(temp.right);
+            x.left = temp.left;
+        }
+        return x;
+    }
+
+    private Node min(Node x) {
+        while (x.left != null) x = x.left;
+        return x;
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        return x;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new BSTIterator();
+    }
+
+    private class BSTIterator implements Iterator<Entry<K, V>> {
+        private Stack<Node> stack = new Stack<>();
+
+        public BSTIterator() {
+            pushLeft(root); //2. Инициализация in-order traversal
+        }
+
+        private void pushLeft(Node x) {
+            while (x != null) {
+                stack.push(x);
+                x = x.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            Node current = stack.pop();
+            pushLeft(current.right);
+            return new Entry<>(current.key, current.val);
+        }
+    }
+}
